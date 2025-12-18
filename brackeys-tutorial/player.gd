bcf5d165f294483f6b21c98be7b1ea611bf4a1ad
David <161796631+guffxx@users.noreply.github.com>
2025-12-17 18:14:55 -0800

    back up

--- brackeys-tutorial/player.gd
+++ brackeys-tutorial/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+var jumpCounter = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -10,15 +11,18 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		jumpCounter += 1
+		while jumpCounter % 3 == 0:
+			velocity.y = JUMP_VELOCITY
+			
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
