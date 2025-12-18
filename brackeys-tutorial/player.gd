d6aae4b280a2eae5534638c9d3f33d6d67356cb5
David <161796631+guffxx@users.noreply.github.com>
2025-12-17 19:10:16 -0800

    Update player.gd

--- brackeys-tutorial/player.gd
+++ brackeys-tutorial/player.gd
@@ -12,10 +12,7 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		jumpCounter += 1
-		while jumpCounter % 3 == 0:
-			velocity.y = JUMP_VELOCITY
-			
+		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
